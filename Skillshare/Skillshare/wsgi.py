9117acbac8f59d131d197ab3bf62653ccc465870
keshav <keshavrokaya1244@gmail.com>
2025-07-08 21:39:44 +0545

    some final changes for deployment

--- Skillshare/Skillshare/wsgi.py
+++ Skillshare/Skillshare/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Skillshare.settings')
 
+# Setup Django
+django.setup()
+
+# Run migrations and collect static files automatically
+try:
+    call_command('migrate', interactive=False)
+    call_command('collectstatic', interactive=False, verbosity=0)
+except Exception as e:
+    # Log the error or pass
+    print(f"Error running migrations or collectstatic: {e}")
+
 application = get_wsgi_application()
+
